5659c92db7b9743db91d3a18542d5b7c2aae98be
DalatCoder <hieuntctk42@gmail.com>
2019-11-09 22:21:17 +0700

    Bo sung menu va lenh nhay

--- lab01.asm
+++ lab01.asm
@@ -1,16 +1,17 @@
 .MODEL SMALL
 .STACK
 .DATA
-
-    GOODBYE DB 10,13, 'Bam phim bat ki de tiep tuc...$'
-    
+    PROMPT DB 10,13, 'Chon bai tap de thuc hien tu 1 den 5 (0 de thoat): $'
+    ERROR_MSG DB 10,13, 'Lua chon khong hop le!$'
+    GOODBYE DB 10,13, 'Bam phim bat ki de tiep tuc...$'         
+         
     ; Bai 1
-    MSG1 DB 'Hello World!$'
+    MSG1 DB 10,13, 'Hello World!$'
     MSG2 DB 10,13, 'Hello, solar system!$'
     MSG3 DB 10,13, 'Hello, universe!$'
     
     ; Bai 2
-    PROMPT1 DB 'Hay nhap mot ky tu: $'
+    PROMPT1 DB 10,13, 'Hay nhap mot ky tu: $'
     CHAR DB ?
     RESULT_MSG1 DB 10,13, 'Ky tu da nhap: $' 
     
@@ -23,7 +24,7 @@
     RESULT_MSG5 DB ', dung sau: $' 
     
     ; Bai 5
-    PROMPT2 DB 'Nhap ky tu thu nhat: $'
+    PROMPT2 DB 10,13, 'Nhap ky tu thu nhat: $'
     PROMPT3 DB 10,13, 'Nhap ky tu thu hai: $'
     RESULT_MSG6 DB 10,13, 'Ky tu tong: $'
     CHAR1 DB ?
@@ -36,151 +37,196 @@ mov ax, @DATA
 mov ds, ax
 
 ; ===============================
-; BAI 1
-; Xuat chuoi 1
-; mov ah, 9
-; lea dx, MSG1
-; int 21h
-
-; Xuat chuoi 2
-; mov ah, 9
-; lea dx, MSG2
-; int 21h
-
-; Xuat chuoi 3
-; mov ah, 9
-; lea dx, MSG3
-; int 21h
-
-; ===============================
-
-; ===============================
-; BAI 2
-;  mov ah, 9
-;  lea dx, PROMPT1
-;  int 21h
-  
-  ; Read character from standard input, result is stored in AL
-;  mov ah, 1
-;  int 21h
-  
-  ; Luu gia tri vua nhap vao CHAR
-;  mov CHAR, al
-  
-;  mov ah, 9
-;  lea dx, RESULT_MSG1
-;  int 21h
-  
-  ; Write character to standard output
-;  mov ah, 6
-;  mov dl, CHAR
-;  int 21h
-                                                                   
-; ===============================
-
-; ===============================
-; BAI 3
-
-;    mov ah, 9
-;    lea dx, PROMPT1
-;    int 21h
-    
-    ; Read character from standard input
-;    mov ah, 1
-;    int 21h
-    
-    ; Luu gia tri vua nhap vao CHAR
-;    mov CHAR, al
-    
-    ; Xuat thong bao ky tu dung truoc
-;    mov ah, 9
-;    lea dx, RESULT_MSG2
-;    int 21h
-    
-    ; Gan CHAR vao al
-;    mov al, CHAR
-    
-    ; Giam gia tri tai al de lay ky tu phia truoc
-;    dec al
-    
-    ; Write character to standard output
-;    mov ah, 6
-;    mov dl, al
-;    int 21h
-    
-    ; Xuat thong bao ky tu dung sau
-;    mov ah, 9
-;    lea dx, RESULT_MSG3
-;    int 21h
-    
-    ; Gan CHAR vao al
-;    mov al, CHAR
-    
-    ; Tang gia tri tai al de lay ky tu phia sau
-;    inc al
-    
-    ; Xuat ky tu ra man hinh
-;    mov ah, 6
-;    mov dl, al
-;    int 21h
-
-; ===============================
-
-; ===============================
-; BAI 4
-    ; Xuat thong bao yeu cau nhap ki tu
-;    mov ah, 9
-;    lea dx, PROMPT1
-;    int 21h
+MAIN:
+    ; Xuat thong bao yeu cau nguoi dung lua chon
+    mov ah, 9
+    lea dx, PROMPT
+    int 21h
     
     ; Nhap ki tu tu ban phim
-;    mov ah, 1
-;    int 21h
+    mov ah, 1
+    int 21h
+    mov CHAR, al
+
+    ; Kiem tra dieu kien lap    
+    CMP CHAR, '0'
+    JE FINISH
     
-    ; Gan ki tu vua nhap vao CHAR
-;    mov CHAR, al
+    ; Thuc thi lenh tuong ung
+    CMP CHAR, '1'
+    JE BAI_1
+    
+    CMP CHAR, '2'
+    JE BAI_2
+    
+    CMP CHAR, '3'
+    JE BAI_3
+    
+    CMP CHAR, '4'
+    JE BAI_4
+    
+    CMP CHAR, '5'
+    JE BAI_5 
+    
+    ; Thong bao lua chon khong hop le
+    JMP INVALID_CHOICE
+    
+    
+
+; ================================
+
+INVALID_CHOICE:
+    mov ah, 9
+    lea dx, ERROR_MSG
+    int 21h
+    JMP MAIN
+
+BAI_1:
+    ; Xuat chuoi 1
+    mov ah, 9
+    lea dx, MSG1
+    int 21h
+
+    ; Xuat chuoi 2
+    mov ah, 9
+    lea dx, MSG2
+    int 21h
+
+    ; Xuat chuoi 3
+    mov ah, 9
+    lea dx, MSG3
+    int 21h
+    
+    ; Ve ham main
+    JMP MAIN
+
+BAI_2:
+    ; Xuat thong bao yeu cau nhap 1 ki tu
+    mov ah, 9
+    lea dx, PROMPT1
+    int 21h
+  
+    ; Read character from standard input, result is stored in AL
+    mov ah, 1
+    int 21h
+  
+    ; Luu gia tri vua nhap vao CHAR
+    mov CHAR, al
+    
+    ; Xuat thong bao ket qua
+    mov ah, 9
+    lea dx, RESULT_MSG1
+    int 21h
+  
+    ; Write character to standard output
+    mov ah, 6
+    mov dl, CHAR
+    int 21h
+    
+    ; Ve ham main
+    JMP MAIN
+                                                                   
+BAI_3:
+    ; Xuat thong bao yeu cau nhap ki tu
+    mov ah, 9
+    lea dx, PROMPT1
+    int 21h
+    
+    ; Read character from standard input
+    mov ah, 1
+    int 21h
+    
+    ; Luu gia tri vua nhap vao CHAR
+    mov CHAR, al
     
     ; Xuat thong bao ky tu dung truoc
-;    mov ah, 9
-;    lea dx, RESULT_MSG2
-;    int 21h
+    mov ah, 9
+    lea dx, RESULT_MSG2
+    int 21h
+    
+    ; Gan CHAR vao al
+    mov al, CHAR
+    
+    ; Giam gia tri tai al de lay ky tu phia truoc
+    dec al
+    
+    ; Write character to standard output
+    mov ah, 6
+    mov dl, al
+    int 21h
+    
+    ; Xuat thong bao ky tu dung sau
+    mov ah, 9
+    lea dx, RESULT_MSG3
+    int 21h
+    
+    ; Gan CHAR vao al
+    mov al, CHAR
+    
+    ; Tang gia tri tai al de lay ky tu phia sau
+    inc al
+    
+    ; Xuat ky tu ra man hinh
+    mov ah, 6
+    mov dl, al
+    int 21h   
+    
+    ; Ve ham main
+    JMP MAIN
+
+BAI_4:
+    ; Xuat thong bao yeu cau nhap ki tu
+    mov ah, 9
+    lea dx, PROMPT1
+    int 21h
+    
+    ; Nhap ki tu tu ban phim
+    mov ah, 1
+    int 21h
+    
+    ; Gan ki tu vua nhap vao CHAR
+    mov CHAR, al
+    
+    ; Xuat thong bao ky tu dung truoc
+    mov ah, 9
+    lea dx, RESULT_MSG2
+    int 21h
     
     ; Xuat ki tu dung truoc
-;    mov al, CHAR
-;    dec al
+    mov al, CHAR
+    dec al
     
-;    mov ah, 2
-;    mov dl, al
-;    int 21h
-    
+    mov ah, 2
+    mov dl, al
+    int 21h
+   
     ; Xuat thong bao ki tu da nhap
-;    mov ah, 9
-;    lea dx, RESULT_MSG4
-;    int 21h
+    mov ah, 9
+    lea dx, RESULT_MSG4
+    int 21h
     
     ; Xuat ki tu da nhap
-;    mov ah, 2
-;    mov dl, CHAR
-;    int 21h
+    mov ah, 2
+    mov dl, CHAR
+    int 21h
     
     ; Xuat thong bao ki tu dung sau
-;    mov ah, 9
-;    lea dx, RESULT_MSG5
-;    int 21h
+    mov ah, 9
+    lea dx, RESULT_MSG5
+    int 21h
     
     ; Xuat ki tu dung sau
-;    mov al, CHAR
-;    inc al
+    mov al, CHAR
+    inc al
     
-;    mov ah, 2
-;    mov dl, al
-;    int 21h
+    mov ah, 2
+    mov dl, al
+    int 21h  
     
-
-; ===============================
-
-; ===============================
-; BAI 5
+    ; Ve ham main
+    JMP MAIN
+    
+BAI_5:
     ; Xuat thong bao yeu cau nhap ki tu thu nhat
     mov ah, 9
     lea dx, PROMPT2
@@ -213,22 +259,25 @@ mov ds, ax
     ; Xuat tong ra man hinh
     mov ah, 2
     mov dl, al
-    int 21h
+    int 21h 
     
+    ; Ve ham main
+    JMP MAIN
     
 ; ===============================
 
 ; Xuat thong bao ket thuc chuong trinh
-mov ah, 9
-lea dx, GOODBYE
-int 21h
+FINISH:
+    mov ah, 9
+    lea dx, GOODBYE
+    int 21h
 
-; Dung chuong trinh xem ket qua
-mov ah, 1
-int 21h
+    ; Dung chuong trinh xem ket qua
+    mov ah, 1
+    int 21h
 
-; Tra quyen dieu hanh cho DOS
-mov ah, 4ch                              
-int 21h
+    ; Tra quyen dieu hanh cho DOS
+    mov ah, 4ch                              
+    int 21h
 
 END
