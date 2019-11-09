40d2a6556742d6864a9125a571751a3575133ae2
DalatCoder <hieuntctk42@gmail.com>
2019-11-09 21:05:21 +0700

    Hoan thanh Bai02 - Lab01

--- lab01.asm
+++ lab01.asm
@@ -1,27 +1,67 @@
 .MODEL SMALL
-
+.STACK
 .DATA
-    NUM DW 1234H, 0F234H
-    SUM DW 2 DUP(0)
+    ; Bai 1
+    MSG1 DB 'Hello World!$'
+    MSG2 DB 10,13, 'Hello, solar system!$'
+    MSG3 DB 10,13, 'Hello, universe!$'
+    
+    ; Bai 2
+    PROMPT1 DB 'Hay nhap mot ky tu: $'
+    CHAR DB ?
+    RESULT_MSG1 DB 10,13, 'Ky tu da nhap: $'
     
 .CODE
-START:
-    MOV AX, @DATA
-    MOV DS, AX
-    MOV AX, NUM
-    MOV BX, 0H
-    ADD AX, NUM+2
-    
-    JNC DOWN
-    
-    INC BX
-    
-    DOWN:
-        MOV SUM,AX
-        MOV SUM+2, BX
-        
-    MOV AH, 4CH
-    INT 21H
-END START
-END        
-    
+
+; Lay dia chi vung du lieu dat vao thanh ghi DS
+mov ax, @DATA
+mov ds, ax
+
+; ===============================
+; BAI 1
+; Xuat chuoi 1
+; mov ah, 9
+; lea dx, MSG1
+; int 21h
+
+; Xuat chuoi 2
+; mov ah, 9
+; lea dx, MSG2
+; int 21h
+
+; Xuat chuoi 3
+; mov ah, 9
+; lea dx, MSG3
+; int 21h
+
+; ===============================
+
+; ===============================
+; BAI 2
+  mov ah, 9
+  lea dx, PROMPT1
+  int 21h
+  
+  ; Read character from standard input, result is stored in AL
+  mov ah, 1
+  int 21h
+  
+  ; Luu gia tri vua nhap vao CHAR
+  mov CHAR, al
+  
+  mov ah, 9
+  lea dx, RESULT_MSG1
+  int 21h
+  
+  ; Write character to standard output
+  mov ah, 6
+  mov dl, CHAR
+  int 21h
+                                                                   
+; ===============================
+
+; Tra quyen dieu hanh cho DOS
+mov ah, 4ch                              
+int 21h
+
+END
