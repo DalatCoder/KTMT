5444b95f10f005e48326ee4d05951e3dd4c23c47
DalatCoder <hieuntctk42@gmail.com>
2019-11-15 22:04:17 +0700

    Sua bai 03

--- Bai-tap-tren-lop/Bitwise-operator/Bai3.asm
+++ Bai-tap-tren-lop/Bitwise-operator/Bai3.asm
@@ -1,64 +1,45 @@
-.model small
-.stack 100h
-.data
-    MSG1 DB 10,13, 'Nhap vao 1 so >> $'
-    MSG2 DB 10,13, 'Chuoi nhi phan tuong ung >> $'
-    MSG3 DB 10,13, 'Bam phim bat ky de tiep tuc...$'
-.code
-
-; Lay du lieu
-mov ax, @data
-mov ds, ax
+.MODEL SMALL
+.STACK 100h
+.DATA
 
 
-input:  mov ah, 9
-        lea dx, MSG1
-        int 21h
-        
-        mov ah, 1
-        int 21h
-        
-        cmp al, '0'
-        jb input
-        
-        cmp al, '9'
-        ja input
-        
-        and al, 0Fh
-        mov bl, al
 
-; thong bao ket qua
-mov ah, 9
-lea dx, msg2
-int 21h
-
-; print result in binary:
-mov cx, 8
-print: mov ah, 2   ; print function.
-       mov dl, '0'
-       test bl, 10000000b  ; test first bit.
-       jz zero
-       mov dl, '1'
-       
-zero:  int 21h
-       shl bl, 1
-loop print
-
-; print binary suffix:
-mov dl, 'b'
-int 21h
-
-; Dung chuong trinh xem ket qua
-mov ah, 9
-lea dx, msg3
-int 21h
-
-mov ah, 1
-int 21h
-
-mov ah, 4ch
-int 21h
-
-end
-
-
+.CODE 
+   ; Lay du lieu tu DATA vao DS
+   MOV AX, @DATA
+   MOV DS, AX
+   
+   ; ==========================================
+   
+   
+     
+   MOV BX, 1234h; 0001 0010 0011 0100
+   MOV CX, 16
+   
+PRINT:
+    SHL BX, 1
+    JNC PRINT_ZERO
+    JC PRINBT_ONE 
+    
+    
+PRINT_ZERO:
+    MOV AH, 2
+    MOV DL, '0'
+    INT 21h
+    JMP EXIT
+    
+PRINT_ONE:
+    MOV AH, 2
+    MOV DL, '1'
+    INT 21h
+    
+EXIT:    
+    LOOP PRINT        
+     
+     
+   ; ===========================================
+   ; Thoat chuong trinh
+   MOV AH, 4Ch
+   INT 21h
+   
+END
