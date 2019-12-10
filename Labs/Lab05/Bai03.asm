94aba4cb7ad3ae40f9f80db648fb97d4b1a8f4e0
DalatCoder <hieuntctk42@gmail.com>
2019-12-10 12:16:13 +0700

    Bo sung procedure

--- Labs/Lab05/Bai03.asm
+++ Labs/Lab05/Bai03.asm
@@ -1,15 +1,9 @@
-; =========================================
-; NGUYEN TRONG HIEU - 1812756 - CTK42   
-; =========================================
-
 .MODEL SMALL
 .STACK 100h
 .DATA
 
     MSG1 DB 10,13, 'Nhap vao 1 day thap luc phan: $'
     MSG2 DB 10,13, 'Day thap luc phan vua nhap la: $'
-    ERR1 DB 10,13, 'Nhap sai! Nhap lai toan bo di, ahihi$'
-    ERR2 DB 10,13, 'Da du 4 gia tri hexa!$'
     END1 DB 10,13, 'Bam phim bat ki de thoat chuong trinh...$'
 
 .CODE
@@ -18,70 +12,94 @@
     MOV DS, AX    
     ; =========================================
     
-    RESET:         
-        ; Hien thi thong bao nhap chuoi hexa
-        MOV AH, 9
+    NHAP_16:                       
         LEA DX, MSG1
+        CALL XUAT_CHUOI
+    
+        CALL NHAP_HEXA
+        CMP DX, 0
+        JE NHAP_16
+        
+    LEA DX, MSG2
+    CALL XUAT_CHUOI
+    
+    CALL XUAT_HEXA        
+                               
+                           
+    ; =========================================
+    ; DUNG MAN HINH, XEM KET QUA
+    CALL THOAT_CHUONG_TRINH
+
+    ; =========================================
+    ; KHAI BAO THU TUC
+    ; =========================================
+    XUAT_CHUOI PROC
+        PUSH AX
+        
+        MOV AH, 9
         INT 21h
         
-        ; Khoi tao gia tri
+        POP AX
+        RET
+    XUAT_CHUOI ENDP
+    
+    NHAP_HEXA PROC
+        PUSH AX
+        PUSH CX
+        
         XOR BX, BX
         XOR CX, CX
-    
-    INPUT:
-        MOV AH, 1
-        INT 21h
+        MOV DX, 1
         
-        ; Kiem tra ENTER => Hien thi day hexa
-        CMP AL, 0Dh
-        JE BREAK
-        
-        CMP AL, '0'
-        JB XULY_LOI
-        CMP AL, '9'
-        JBE XULY_SO
-        CMP AL, 'A'
-        JB XULY_LOI
-        CMP AL, 'F'
-        JBE XULY_CHU
-        
-        XULY_LOI:
-            MOV AH, 9
-            LEA DX, ERR1
+        INPUT:
+            MOV AH, 1
             INT 21h
-            JMP RESET
             
-        XULY_SO:    
-            AND AL, 0Fh
-            JMP LUU
-        
-        XULY_CHU:
-            SUB AL, 37h
+            CMP AL, 0Dh
+            JE BREAK
             
-        LUU:
-            SHL BX, 4
-            OR BL, AL
+            CMP AL, '0'
+            JB XULY_LOI
+            CMP AL, '9'
+            JBE XULY_SO
+            CMP AL, 'A'
+            JB XULY_LOI
+            CMP AL, 'F'
+            JBE XULY_CHU
             
-            INC CX
+            XULY_LOI:
+                MOV DX, 0
+                XOR BX, BX
+                JMP BREAK
+                
+            XULY_SO:
+                AND AL, 0Fh
+                JMP XULY
+                
+            XULY_CHU: 
+                SUB AL, 37h
+                
+            XULY:   
+                SHL BX, 4
+                OR BL, AL
+                
+                INC CX
+                
+                CMP CX, 4
+                JB INPUT                                    
             
-        ; Kiem tra CX va thoat chuong trinh
-        CMP CX, 4
-        JB INPUT
-        
-        
-        ; Xuat thong bao da nhap du 4 ki tu
-        MOV AH, 9
-        LEA DX, ERR2
-        INT 21h
+        BREAK:            
+            POP CX
+            POP AX
+            RET
+    NHAP_HEXA ENDP
     
-    BREAK:
-        ; Xuat thong bao ket qua
-        MOV AH, 9
-        LEA DX, MSG2
-        INT 21h
+    XUAT_HEXA PROC
+        PUSH AX
+        PUSH CX
+        PUSH DX
         
         MOV CX, 4
-        
         PRINT:
             MOV DL, BH
             SHR DL, 4
@@ -91,30 +109,38 @@
             JA XUAT_CHU
             
             XUAT_SO:
-                MOV AH, 2 
+                MOV AH, 2
                 OR DL, 30h
                 INT 21h
-                JMP CONTINUE
-                
+                JMP CONTINUE                        
+                       
             XUAT_CHU:
                 MOV AH, 2
                 ADD DL, 37h
-                INT 21h
+                INT 21h                
             
             CONTINUE:
                 ROL BX, 4
-                LOOP PRINT
-    ; =========================================
-    ; DUNG MAN HINH, XEM KET QUA
-    MOV AH, 9
-    LEA DX, END1
-    INT 21h
+        LOOP PRINT
+                
+        POP DX
+        POP CX
+        POP AX                
+        RET
+    XUAT_HEXA ENDP
     
-    MOV AH, 7
-    INT 21h
-    
-    ; THOAT CHUONG TRINH
-    MOV AH, 4Ch
-    INT 21h
-    
+    THOAT_CHUONG_TRINH PROC
+        MOV AH, 9
+        LEA DX, END1
+        INT 21h
+        
+        MOV AH, 7
+        INT 21h
+        
+        ; THOAT CHUONG TRINH
+        MOV AH, 4Ch
+        INT 21h
+
+        RET
+    THOAT_CHUONG_TRINH ENDP
 END
